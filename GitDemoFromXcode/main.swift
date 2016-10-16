0278dd4d0fb3e7fb6c4de69e092ed4daa282fa0a
yghhust <ghy_hust@qq.com>
2016-10-16 23:18:15 +0800

    Init

---
+++ GitDemoFromXcode/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  GitDemoFromXcode
+//
+//  Created by yuguohua on 16/10/16.
+//  Copyright © 2016年 yuguohua. All rights reserved.
+//
+
+import Foundation
+
+//let a＝10;
+//var b=12.0;
+print("Hello, World!");
+
